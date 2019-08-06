0df5e55092469038f6e4c663bdec40917e8b8f4a
maskaywalanay <mwalanay@gmail.com>
2019-08-06 22:19:25 +0700

    first commit

---
+++ SusunanLayout/app/src/main/java/santai/malam/voss/MainActivity.java
@@ -0,0 +1,14 @@
+package santai.malam.voss;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
